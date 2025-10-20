dee228e0d87b9498c83c681a783fc9875ce7bcaa
yanotahck <yetanotherdingus@gmail.com>
2025-10-20 02:21:53 -0400

    make main.py ready for a self contained application

--- main.py
+++ main.py
@@ -1,32 +1,4 @@
-from src.encode import embed_in_wav
-from src.decode import extract_from_wav
-from src.encryption import encrypt_bytes, decrypt_bytes
-import gzip
-import numpy as np
+# most complicated file in this project ngl
+from src.gui import runGui
 
-def test_signal_mux():
-    password = "averysecurepassphrase"
-    infile = "secret.txt"
-    carrier_in = "example_2p2e5.mp3"
-    carrier_out = "secretdata.wav"
-    seed = 42
-
-    with open(infile, "rb") as f:
-        raw = f.read()
-    comp = gzip.compress(raw)
-    enc = encrypt_bytes(password, comp)
-    length = len(enc).to_bytes(4, "big")
-    payload = length + enc
-
-    embed_in_wav(carrier_in, carrier_out, payload, seed=seed)
-
-    header = extract_from_wav(carrier_out, 4, seed=seed)
-    length = int.from_bytes(header, "big")
-    blob = extract_from_wav(carrier_out, 4 + length, seed=seed)[4:4+length]
-    dec = decrypt_bytes(password, blob)
-    raw = gzip.decompress(dec)
-
-    print("Recovered:", raw)
-
-if __name__ == "__main__":
-    test_signal_mux()
+runGui()
